d93836f53ff01a9717e8b032e2370cfa3d7d274b
GustavoCarvalho25 <gustavo.carvalhoas@hotmail.com>
2022-04-04 19:06:42 -0300

    Commit tasks [04/04/2022] {19:06}

---
+++ CSharp/List 3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace List_3
+{
+    class Program
+    {   
+
+        static void Main(string[] args)
+        {
+            
+            float altura;
+            float peso;
+            float imc;
+
+            Console.WriteLine("Qual a sua altura?");
+            altura = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nQual o seu peso?");
+            peso = float.Parse(Console.ReadLine());
+
+            imc = peso / (altura * altura);
+
+
+            Console.WriteLine($"\nSeu IMC é: {imc}");
+        }
+    }
+}
