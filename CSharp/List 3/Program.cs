d0617e8f1558b918b9e6224ff001cae3576f505e
GustavoCarvalho25 <gustavo.carvalhoas@hotmail.com>
2022-04-05 15:42:30 -0300

    Commit task imc [05/04/2022] {15:42}

--- CSharp/List 3/Program.cs
+++ CSharp/List 3/Program.cs
@@ -11,17 +11,34 @@ namespace List_3
             float altura;
             float peso;
             float imc;
+            string classPeso;
 
-            Console.WriteLine("Qual a sua altura?");
+            Console.WriteLine("Qual a sua altura em metros?");
             altura = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("\nQual o seu peso?");
+            Console.WriteLine("\nQual o seu peso em kg?");
             peso = float.Parse(Console.ReadLine());
 
             imc = peso / (altura * altura);
 
+            if (imc < 18.5)
+            {
+                classPeso = "abaixo do peso";
+            }
+            else if (imc < 25)
+            {
+                classPeso = "com peso normal";
+            }
+            else if (imc < 30)
+            {
+                classPeso = "em sobrepeso";
+            }
+            else
+            {
+                classPeso = "em obesidade";
+            }
 
-            Console.WriteLine($"\nSeu IMC é: {imc}");
+            Console.WriteLine($"\nVoce esta {classPeso}");
         }
     }
 }
