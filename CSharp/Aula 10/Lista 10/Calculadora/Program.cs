707cca7a9dc37572172e3d9460ef9aa559e05259
GustavoCarvalho25 <gustavo.carvalhoas@hotmail.com>
2022-04-12 14:58:23 -0300

    @GustavoCarvalho25 - Atividade 10 [12/04/2022] {14:40}

---
+++ CSharp/Aula 10/Lista 10/Calculadora/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Calculadora.src;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("Escreva o tamanho dos lados do quadrado");
+            double ladoQuadrado = double.Parse(Console.ReadLine());
+            Quadrado quadrado = new Quadrado("quadrado", ladoQuadrado, ladoQuadrado);
+            Console.WriteLine($"A base do {quadrado.Nome} é {quadrado.Base} e a altura é {quadrado.Altura}\nA area é {quadrado.CalcularArea()}");
+
+            Console.WriteLine("Escreva o tamanho da base do retangulo");
+            double baseRetangulo = double.Parse(Console.ReadLine());
+            Console.WriteLine("Escreva o tamanho da altura do retangulo");
+            double alturaRetangulo = double.Parse(Console.ReadLine());
+            Retangulo retangulo = new Retangulo("retangulo", baseRetangulo, alturaRetangulo);
+            Console.WriteLine($"A base do {retangulo.Nome} é {retangulo.Base} e a altura é {retangulo.Altura}\nA area é {retangulo.CalcularArea()}");
+
+            Console.WriteLine("Escreva o tamanho da base do triangulo");
+            double baseTriangulo = double.Parse(Console.ReadLine());
+            Console.WriteLine("Escreva o tamanho da altura do triangulo");
+            double alturaTriangulo = double.Parse(Console.ReadLine());
+            Triangulo triangulo = new Triangulo("triangulo", baseTriangulo, alturaTriangulo);
+            Console.WriteLine($"A base do {triangulo.Nome} é {triangulo.Base} e a altura é {triangulo.Altura}\nA area é {triangulo.CalcularArea()}");
+        }
+    }
+}
---
+++ CSharp/Aula 10/Lista 10/Zoologico/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Zoologico.src;
+
+namespace Zoologico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //nova forma de implementar listas
+            List<Animal> animais = new List<Animal>(
+                new Animal[]{
+                    new Gato("Gato felix", "Cinza", "Felino"),
+                    new Cachorro("Cachorro RoRo", "Preto", "Canídio"),
+                    new Cachorro("Cachorro Nino", "Amarelo", "Canídio"),
+                    new Gato("Gato Doritos", "Laranja", "Felino"),
+                    new Macaco("Zeca", "Marrom", "Primata")
+                }
+            );
+
+            foreach (Animal animal in animais)
+            {
+                if (animal.Classificacao == "Canídio")
+                {
+                    Console.WriteLine($"{animal.Nome} é um {animal.Classificacao}");
+                    animal.Comunicar("Au au!");
+                }
+                else if (animal.Classificacao == "Felino")
+                {
+                    Console.WriteLine($"{animal.Nome} é um {animal.Classificacao}");
+                    animal.Comunicar("Miau!");
+                }
+                else if (animal.Classificacao == "Primata")
+                {
+                    Console.WriteLine($"{animal.Nome} é um {animal.Classificacao}");
+                    animal.Comunicar("Uhuuu HAHAHA!");
+                }
+            }
+        }
+    }
+}
